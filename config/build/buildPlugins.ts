726ba8c1b5d93525adc072c11772ae3eea4f9c57
masleeh <102211370+masleeh@users.noreply.github.com>
2023-09-11 16:18:34 +0300

    fix pipeline

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,22 +5,30 @@ import type { IBuildOptions } from "./types/config"
 // @ts-ignore BundleAnalyzer types are not supported for Webpack5
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-const buildPlugins = ({paths, isDev}: IBuildOptions):webpack.WebpackPluginInstance[] => [
-    new HTMLWebpackPlugin({
-        template: paths.html
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css'
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-        openAnalyzer: false
-    })
-]
+const buildPlugins = ({paths, isDev}: IBuildOptions):webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev)
+        })
+    ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }))
+    }
+
+    return plugins
+}
+
 
 export default buildPlugins
